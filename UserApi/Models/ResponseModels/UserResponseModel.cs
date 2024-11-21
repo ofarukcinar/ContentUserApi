2538cd21b310ff45ce395a75857af955bb2e974b
omer <ofarukcinar1@gmail.com>
2024-11-21 14:53:44 +0300

    updated

---
+++ UserApi/Models/ResponseModels/UserResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UserApi.Models.ResponseModels
+{
+	public class UserResponseModel
+	{
+	
+
+               public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    
+	}
+}
+
